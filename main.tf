03c7f8cef1c4b1aedf8acbd6b5468dbe87e9c823
Dan Barr <dan@dbarr.com>
2023-06-04 13:46:24 -0400

    Update dependencies again
    * On second thought, no-code modules should be more explicit since
      there's no parent module to enforce.
    * Add TF version (1.2+ needed for postconditions)

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 terraform {
+  required_version = ">= 1.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.46"
+      version = "~> 0.57"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.1"
+      version = "~> 3.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4"
+      version = "~> 3.4"
     }
   }
 }
